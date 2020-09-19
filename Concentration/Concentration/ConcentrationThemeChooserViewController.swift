b4ea5346e5016d6b1787bf77d2d9f84130f00f0e
ellis <ellis.mage@gmail.com>
2020-09-19 14:33:46 +0800

    9. View Controller Lifecycle 生命周期

--- Concentration/Concentration/ConcentrationThemeChooserViewController.swift
+++ Concentration/Concentration/ConcentrationThemeChooserViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ConcentrationThemeChooserViewController: UIViewController, UISplitViewControllerDelegate {
+class ConcentrationThemeChooserViewController: VCLLoggingViewController, UISplitViewControllerDelegate {
 
+    
+    override var vclLoggingName: String {
+        return "ThemeChooser"
+    }
+    
     let themes = [
         "Sports": "⚽️🏀🏈⚾️🎾🏐🏉🎱🏓⛷🎳⛳️",
         "Animals": "🐶🦆🐹🐸🐘🦍🐓🐩🐦🦋🐙🐏",
@@ -17,22 +22,23 @@ class ConcentrationThemeChooserViewController: UIViewController, UISplitViewCont
     ]
     
     override func awakeFromNib() {
+        super.awakeFromNib()
         splitViewController?.delegate = self
     }
     
     @IBAction func chooseTheme(_ sender: Any) {
-        if let cvc = splitViewDetailConcentrationViewController {
-            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
-                cvc.theme = theme
-            }
-        } else if let cvc = lastSeguedToConcentrationViewController {
-            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
-                cvc.theme = theme
-                navigationController?.pushViewController(cvc, animated: true)
-            }
-        } else {
+//        if let cvc = splitViewDetailConcentrationViewController {
+//            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+//                cvc.theme = theme
+//            }
+//        } else if let cvc = lastSeguedToConcentrationViewController {
+//            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+//                cvc.theme = theme
+//                navigationController?.pushViewController(cvc, animated: true)
+//            }
+//        } else {
             performSegue(withIdentifier: "Choose Theme", sender: sender as! UIButton)
-        }
+//        }
     }
     
     //iPad    
